bd0b11f40bac418c93080f3db6faa41da1521383
JBalajiReddy <jeedipallybalajireddy@gmail.com>
2025-07-19 21:58:30 +0530

    Cleaned and organized Spring Core Maven examples

--- Week3_Spring_Core_Maven/Exercise1/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-  public void printBookList() {
-    System.out.println("Book list fetched from repository.");
-  }
-}
--- Week3_Spring_Core_Maven/Exercise2/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-public class BookRepository {
-  public void printBookList() {
-    System.out.println("Fetching book list from repository...");
-    System.out.println("Book list fetched from repository.");
-  }
-}
